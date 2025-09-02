f86507b241e5380d47d2fbb81a44b0024dbbe5a2
Darshan Reddy <darshan.puli1166@gmail.com>
2025-09-02 23:34:04 +0530

    please save me again again

--- src/main/java/org/example/DaoImplementation/MaturityBucketDaoImplementation.java
+++ src/main/java/org/example/DaoImplementation/MaturityBucketDaoImplementation.java
@@ -12,6 +12,8 @@ public class MaturityBucketDaoImplementation implements MaturityBucketDao {
     private final Connection connection = OracleDbConnection.getConnection();
     private static final String FIND_MATURITY_BUCKET = "select * from maturity_buckets where ?>=start_range and ?<=end_range";
     private static final String UPDATE_MATURITY_BUCKET = "UPDATE maturity_bucket SET total_assets_value = ?, net_gap = ? WHERE bucket_id = ?";
+    private static final String GET_MATURITY_BUCKET = "select * from maturity_buckets";
+
     public MaturityBucketDaoImplementation() throws SQLException {
     }
 
@@ -43,6 +45,24 @@ public class MaturityBucketDaoImplementation implements MaturityBucketDao {
         }catch(Exception e){
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    public void getMaturityBuckets(){
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(GET_MATURITY_BUCKET);
+            while (resultSet.next()) {
+                MaturityBucket maturityBucket = new MaturityBucket();
+                maturityBucket.setBucketID(resultSet.getObject("bucket_id", UUID.class));
+                maturityBucket.setBucketName(resultSet.getString("bucket_name"));
+                maturityBucket.setTotalAssetsValue(resultSet.getInt("total_assets_value"));
+                maturityBucket.setTotalLiabilitiesValue(resultSet.getInt("total_liabilities_value"));
+                maturityBucket.setNetGap(resultSet.getInt("net_gap"));
+                System.out.println(maturityBucket);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
