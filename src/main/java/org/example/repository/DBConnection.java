7688b97befd9ad6edcf4c9b80b47cdf687214907
Rohit Sridharan <rohitdhoni4@gmail.com>
2025-09-08 19:15:30 +0530

    removed extra files

--- src/main/java/org/example/repository/DBConnection.java
+++ src/main/java/org/example/repository/DBConnection.java
@@ -12,11 +12,9 @@ public class DBConnection {
 
     public static Connection getConnection() {
         Connection connection = null;
-<<<<<<< HEAD
-
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            connection = DriverManager.getConnection("jdbc:oracle:thin:@10.120.159.196:1521:free","system","root");
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@0.0.0.0:1521:free","system","root");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -24,21 +22,5 @@ public class DBConnection {
 
         return connection;
     }
-=======
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/alm?createDatabaseIfNotExist=true",
-                "root",
-                "rohit"
-            );
 
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return connection;
-    }
-
-
->>>>>>> f03904fa03a6bd15d86b7e9223532f88c9489127
 }
